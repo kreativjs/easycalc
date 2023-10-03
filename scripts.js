92c38281ef73297701f29c1e74a7afbab09d9e84
Andrei Benea <andrei.benea89@gmail.com>
2023-10-03 14:49:23 +0300

    appears to be working.. no percentage yet

--- scripts.js
+++ scripts.js
@@ -1,135 +1,136 @@
-const numberButtons = document.querySelectorAll('.operand')
-const operatorButtons = document.querySelectorAll('.operator')
-const decimalButton = document.querySelector('.decimal')
-const clearButton = document.querySelector('.clear')
-const invertButton = document.querySelector('.sign')
-const percentButton = document.querySelector('.percent')
-const displayed = document.querySelector('#display')
-const displayedInt = parseInt(displayed.textContent)
+const numberButtons = document.querySelectorAll('.operand');
+const operatorButtons = document.querySelectorAll('.operator');
+const decimalButton = document.querySelector('.decimal');
+const clearButton = document.querySelector('.clear');
+const invertButton = document.querySelector('.sign');
+const percentButton = document.querySelector('.percent');
+const equalsButton = document.querySelector('#equals');
+const displayed = document.querySelector('#display');
+const displayedInt = parseInt(displayed.textContent);
+const displayedString = displayed.textContent;
 
-let a, b;
-let memory = displayedInt;
+let memory = displayedString;
+let secondNumber = 0;
+let complete = false;
+let operator = '';
+let result;
 
-console.log(memory)
+console.log(memory);
+console.log(typeof memory);
 
-let addition = (a, b) => memory += a + b
-let subtraction = (a, b) => memory += a - b
-let multiplication = (a, b) => memory += a * b
-let division = (a, b) => memory += a / b
-let clearAll = () => {
-    a = 0
-    b = 0
-    memory = 0
-    updateDisplay()
-}
+let addition = () => result = parseFloat(memory) + parseFloat(secondNumber);
+let subtraction = () => result = parseFloat(memory) - parseFloat(secondNumber);
+let multiplication = () => result = parseFloat(memory) * parseFloat(secondNumber);
+let division = () => result = parseFloat(memory) / parseFloat(secondNumber);
 
 let updateDisplay = () => {
-    document.getElementById('display').innerText = memory;
-}
-
-let addDecimal = () => {
-    memory += '.'
-    console.log(memory)
-    updateDisplay()
+    if (operator === '') {
+        document.getElementById('display').innerText = memory;
+    }
+    else {
+        document.getElementById('display').innerText = secondNumber;
+    }
 }
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        let buttonInt = parseInt(button.value)
-        console.log(button.value)
-        memory = buttonInt
-        updateDisplay()
-    })
-})
-
-invertButton.addEventListener('click', () => {
-    memory *= -1;
-    console.log(memory)
-    updateDisplay()
-})
-
-clearButton.addEventListener('click', () => {
-    clearAll()
-    updateDisplay()
-})
-
-decimalButton.addEventListener('click', () => {
-    addDecimal()
-    updateDisplay()
-})
-
-operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        switch (button.value) {
-            case "/":
-                console.log('You pressed /')
-                break;
-            case "*":
-                console.log('You pressed *')
-                break;
-            case "-":
-                console.log('You pressed -')
-                break;
-            case "+":
-                console.log('You pressed +')
-                break;
+        let memoryInt = parseFloat(memory);
+        if (operator === '') {
+            if (memoryInt === 0 && complete == false) {
+                memory = button.value;
+                console.log('The first number is: ' + memory);
+                console.log('The first number is of type: ' + typeof memory);
+            }
+            else {
+                memory += button.value;
+            }
+            updateDisplay();
+        }
+        else {
+            if (secondNumber === 0) {
+                secondNumber = button.value;
+                console.log('The second number is: ' + secondNumber)
+                console.log('The second number is of type: ' + typeof secondNumber)
+            }
+            else {
+                secondNumber += button.value;
+                console.log('The second number is: ' + secondNumber)
+                console.log('The second number is of type: ' + typeof secondNumber)
+            }
+            updateDisplay();
         }
     })
 })
 
-// console.log(numberButtons)
-// console.log(operatorButtons)
-// console.log(displayedInt)
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.value;
+        secondNumber = 0;
+        console.log('New operator selected: ' + operator + ' ... ' + typeof operator)
+    })
+})
 
-// addition(2, 3);
-// console.log('Memory END: ' + memory)
-// console.log('DisplayedInt END: ' + displayedInt)
+decimalButton.addEventListener('click', () => {
+    let memoryString = memory.toString();
+    let secondNumberString = secondNumber.toString();
+    if (operator === '') {
+        if (memoryString.includes('.') == false) {
+            memory += '.';
+            updateDisplay();
+        }
+        else updateDisplay();
+    }
+    else {
+        if (secondNumberString.includes('.') == false) {
+            secondNumber += '.';
+            updateDisplay();
+        }
+        else updateDisplay();
+    }
+})
 
+invertButton.addEventListener('click', () => {
+    memory *= -1;
+    console.log(memory);
+    updateDisplay();
+})
 
+clearButton.addEventListener('click', () => {
+    memory = 0;
+    result = 0;
+    complete = false;
+    operator = '';
+    updateDisplay();
+})
 
-
-
-// let displayedNumber = parseInt(displayedNumberString.textContent)
-
-// console.log(displayedNumberString.textContent)
-// console.log(displayedNumber)
-
-// let storeNumber = (nr) => {
-//     console.log('storing number before... ' + displayedNumber)
-//     displayedNumber + nr
-//     console.log('storing number after... ' + displayedNumber)
-// }
-
-// let memory = 0;
-
-// let clear = () => {
-//     a = 0
-//     b = 0
-//     d = 0
-//     memory = 0
-//     console.log('Memory cleared', a, b, d, memory)
-// }
-
-
-// let getNumber = () => {
-//     let i = 0;
-//     for (i = 0; i < buttonNumber.length; i++) {
-//         let item = buttonNumber[Object.keys(buttonNumber)[i]]
-//         console.log(item.value)
-//     }
-// }
-
-// console.log(getNumber())
-
-
-
-// buttonClear.addEventListener('click', clear);
-
-// let getPressedNr = () => {
-//     const buttonNumber = document.getElementsByClassName('operand');
-//     for (let i = 0; i < buttonNumber.length; i++) {
-//         console.log(buttonNumber[i].value)
-//     }
-// }
-
-// getPressedNr();
+equalsButton.addEventListener('click', () => {
+    switch (operator) {
+        case '/':
+            if (parseFloat(secondNumber) === 0) {
+                result = 1;
+                break;
+            }
+            else {
+                division();
+            }
+            break;
+        case '*':
+            multiplication();
+            break;
+        case '-':
+            subtraction();
+            break;
+        case '+':
+            addition();
+            break;
+        default:
+            console.log('No operator selected');
+            break;
+    }
+    secondNumber = 0;
+    operator = '';
+    memory = result;
+    console.log('Memory on equals: ' + memory);
+    complete = true;
+    document.getElementById('display').innerText = memory;
+})
