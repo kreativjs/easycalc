21734bd01106a16392dbca1c9567744f739f1056
Legrik2 <144489938+Legrik2@users.noreply.github.com>
2023-10-09 09:28:17 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,7 +6,11 @@ const result = document.querySelector('#result'),
       clear = document.querySelector('#clear'),
       ce = document.querySelector('#ce');
 let ex = ''; // the expression string to be eval'd
+let arg;
+let operator = '';
+let secondNumber = '';
 result.innerHTML = '0';
+
 
 
 
@@ -45,12 +49,6 @@ Array.from(operation).forEach(function(element) {
       element.addEventListener('click', clickO);
     });
 
-// clear all on click
-clear.addEventListener('click', () => {
-  result.innerHTML = '';
-  expression.innerHTML = '';
-  ex = '';
-})
 
 // clear last entry on click
 ce.addEventListener('click', () => {
@@ -59,6 +57,7 @@ ce.addEventListener('click', () => {
     expression.innerHTML = doCE(expression.innerHTML);
     ex = doCE(ex); 
     result.innerHTML = 0;
+    expression.innerHTML = 0;
     
     function doCE(arg) {
       arg = arg.split(/([\/\*\+\-\=])/g);
@@ -66,6 +65,38 @@ ce.addEventListener('click', () => {
       return arg.join('');
     }
   }
+})
+
+backspace.addEventListener('click', () => {
+    let display = document.getElementById("result");
+    let currentValue = result.innerText;
+    if (currentValue.length > 1) {
+        result.innerText = currentValue.substring(0, currentValue.length - 1);
+        expression.innerText = currentValue.substring(0, currentValue.length - 1);
+        ex = currentValue.substring(0, currentValue.length - 1);
+        operator = '';
+    } else {
+        result.innerText = '0';
+        expression.innerText = '0';
+        ex = 0; 
+        completed = false;
+        operator = '';        
+       
+    }
+})
+
+sign.addEventListener('click', () => {
+    ex *= -1;
+    ex = String(ex);
+    console.log(ex);
+    result.innerHTML = trim12(ex);
+    expression.innerHTML = trim12(ex);
+})
+
+percent.addEventListener('click', () => {
+    ex *= 0.01;
+    result.innerHTML = trim12(ex);
+    expression.innerHTML = trim12(ex);
 })
 
 // calculate the whole thing
@@ -98,3 +129,58 @@ function trim12(arg) { // if we calculate a number that's too long
     return arg;
   }
 }
+
+document.addEventListener("keydown", function (event) {
+    let key = event.key;
+    if (key === 'Backspace') {
+        if (ex === '') {
+            ex = ex.slice(0, -1);
+        } else if (secondNumber !== '') {
+            secondNumber = secondNumber.slice(0, -1);
+        } else if (operator !== '') {
+            expression.innerHTML = expression.innerHTML.slice(0, -1);
+            result.innerHTML = result.innerHTML.slice(0, -1);
+        }
+    }  
+    if (/^[0-9.]$/.test(key)) {        
+        if (operator === '') {            
+            if (key === '.') {               
+                if (!ex.includes('.')) {
+                    ex += '0.';
+                }
+            } else {               
+                ex += key;
+            }
+        } else {            
+            if (secondNumber === '' && key === '.') {               
+                secondNumber += '0.';
+            } else if (key !== '.') {                
+                secondNumber += key;
+            }
+        }
+        result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {       
+        operator = key;
+        expression.innerHTML += key;
+        ex += key;
+        result.innerHTML = key;
+    } else if (key === 'Enter' || key === '=') {       
+        if (operator && secondNumber !== '') { 
+            // Ваш код для вычисления результата
+        }
+    }
+});
+
+function keyboard(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
+
+
+
